ef56f41fb954383e67d747b66a3b654d7932d514
Ak-lee <laklc@126.com>
2018-01-20 16:25:06 +0800

    画板续

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth=5;
 
 autoSetCanvasSize(yyy)
 
@@ -9,15 +10,63 @@ listenToUser(yyy)
 var eraserEnabled = false
 eraser.onclick = function() {
   eraserEnabled =true
-  actions.className = 'actions x' 
+  eraser.classList.add ('active'); 
+  pen.classList.remove('active');
 }
-brush.onclick = function(){
+pen.onclick = function(){
   eraserEnabled = false
-  actions.className = 'actions'
+  pen.classList.add ('active'); 
+  eraser.classList.remove ('active'); 
 }
-
-
-
+black.onclick=function(){
+  context.fillStyle='black';
+  context.strokeStyle='black';
+  black.classList.add('active');
+  red.classList.remove('active');
+  green.classList.remove('active');
+  blue.classList.remove('active');
+}
+red.onclick=function(){
+  context.fillStyle='red';
+  context.strokeStyle='red';
+  red.classList.add('active');
+  green.classList.remove('active');
+  blue.classList.remove('active');
+  black.classList.remove('active');
+}
+green.onclick=function(){
+  context.fillStyle='green';
+  context.strokeStyle='green';
+  red.classList.remove('active');
+  green.classList.add('active');
+  blue.classList.remove('active');
+  black.classList.remove('active');
+}
+blue.onclick=function(){
+  context.fillStyle='blue';
+  context.strokeStyle='blue';
+  red.classList.remove('active');
+  green.classList.remove('active');
+  blue.classList.add('active');
+  black.classList.remove('active');
+}
+thin.onclick=function(){
+lineWidth=5;
+}
+thick.onclick=function(){
+  lineWidth=10;
+}
+clear.onclick=function(){
+  context.clearRect(0,0,yyy.width,yyy.height);
+}
+download.onclick=function(){
+  var url=yyy.toDataURL("image/png");
+  var a=document.createElement('a');
+  a.href=url;
+  a.download="我的画作";
+  a.target='_blank';
+  a.click();
+}
 /******/
 
 function autoSetCanvasSize(canvas) {
@@ -38,16 +87,14 @@ function autoSetCanvasSize(canvas) {
 
 function drawCircle(x, y, radius) {
   context.beginPath()
-  context.fillStyle = 'black'
   context.arc(x, y, radius, 0, Math.PI * 2);
   context.fill()
 }
 
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'black'
   context.moveTo(x1, y1) // 起点
-  context.lineWidth = 5
+  context.lineWidth = lineWidth;
   context.lineTo(x2, y2) // 终点
   context.stroke()
   context.closePath()
@@ -68,7 +115,7 @@ function listenToUser(canvas) {
       var y = aaa.touches[0].clientY
       using = true
       if (eraserEnabled) {
-        context.clearRect(x - 5, y - 5, 10, 10)
+        context.clearRect(x - 8, y - 8, 16, 16)
       } else {
         lastPoint = {
           "x": x,
